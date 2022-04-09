3ddfb0e9e1aa1474b844df7b72ea1e5af4577643
Daria <dashik-ok@mail.ru>
2022-04-09 21:08:38 +0300

    переделанный коммит

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ console.log(
   createOffers()
 );
 import './popup.js';
+<<<<<<< HEAD
+=======
+
+>>>>>>> 18b3a9cd41c48a6bb495a912e927cdd28c17eb78
 import {differentFieldValues} from './forms.js';
 differentFieldValues();
 
@@ -14,4 +18,7 @@ setTimeout(activatePage, 3000);
 disableMapFilter();
 setTimeout(activateMapFilter, 3000);
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 18b3a9cd41c48a6bb495a912e927cdd28c17eb78
