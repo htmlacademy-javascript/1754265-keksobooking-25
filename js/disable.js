09624e221778c00790642611ce0438079360b3c8
Daria <dashik-ok@mail.ru>
2022-04-18 00:36:39 +0300

    убраны ошибки eslint

--- js/disable.js
+++ js/disable.js
@@ -4,28 +4,28 @@ addForm.classList.add('disabled');
 
 export function disablePage(){
   addForm.querySelectorAll('fieldset').forEach((item) => {
-item.disabled=true;
+    item.disabled=true;
   });
-};
+}
 
 export function activatePage(){
   addForm.querySelectorAll('fieldset').forEach((item) => {
-item.disabled=false;
+    item.disabled=false;
   });
-};
+}
 
 const addFormMapFilter = document.querySelector('.map__filters');
 addFormMapFilter.classList.add('disabled');
 
 export function disableMapFilter(){
   addFormMapFilter.querySelectorAll('.map__filter').forEach((item) => {
-item.disabled=true;
+    item.disabled=true;
   });
-};
+}
 
 export function activateMapFilter(){
   addFormMapFilter.querySelectorAll('.map__filter').forEach((item) => {
-item.disabled=false;
+    item.disabled=false;
   });
-};
+}
 
--- js/util.js
+++ js/util.js
@@ -1,24 +1,26 @@
+const ALERT_SHOW_TIME = 5000;
+
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};  // Результат: целое число из диапазона "от...до" (источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+}  // Результат: целое число из диапазона "от...до" (источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
 getRandomNumber(1, Infinity);
 
 function getRandomResult(min, max, maxNumber = 0) {
   if (min > max || min < 0 || max <= 0) {
     return ('Неверный диапазон!');
-  };
+  }
 
   const numberDegree = 10 ** maxNumber;
   const result = ((Math.random() * (max - min) + min) * numberDegree) / numberDegree;
   return +result.toFixed(maxNumber);
-}; // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой" (источник: https://qna.habr.com/q/999157)
+} // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой" (источник: https://qna.habr.com/q/999157)
 getRandomResult(6, 6, 9);
 
 function getRandomNumbers(from, to) {
   return Math.floor(Math.random() * (to - from + 1)) + from;
-};
+}
 
 function defineRoomWord(count) {
   const roomsCount = Number(count);
@@ -42,16 +44,9 @@ function defineGuestWord(count) {
   return guestWord;
 }
 
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
-const isEscapeKey = (evt) => {
-  return evt.key === 'Escape';
-};
-
-const isEnterKey = (evt) => {
-  return evt.key === 'Enter';
-};
-
-const ALERT_SHOW_TIME = 5000;
+const isEnterKey = (evt) => evt.key === 'Enter';
 
 const showAlert = (message) => {
   const alertContainer = document.createElement('div');
